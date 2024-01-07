95b234ffa9524e7ebe7772e426fd77949f7cb391
Mohana priya M K <145193336+mohanapriyamphil@users.noreply.github.com>
2024-01-07 22:24:21 +0530

    Add files via upload

---
+++ random-fox.js
@@ -0,0 +1,28 @@
+const apiUrl = "https://randomfox.ca/floof/";
+
+const imageArea = document.getElementById("fox-image");
+
+const overlayElement = document.getElementById("overlay");
+
+const loadFoxImage = ({ image }) => {
+  const imgElement = document.getElementById("img-element");
+  imgElement.src = image;
+};
+
+const sleepTime = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("resolved");
+    }, 1000);
+  });
+
+const getRandomFox = async () => {
+  overlayElement.style.display = "flex";
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  loadFoxImage(data);
+  overlayElement.style.display = "none";
+};
+
+getRandomFox();
